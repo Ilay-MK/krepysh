b2c915f2b33a7fda48867e7bce6157ece4895f6f
Ilya Purouski <MiKrob09@gmail.com>
2017-04-03 23:24:33 +0300

    10.	сделать авто воспроизведение видоса на 1 экран вместо фотки; (на мобилках под тезисами, перед "Записаться на бесплатное пробное" и кнопкой);

--- src/assets/js/partials/common.js
+++ src/assets/js/partials/common.js
@@ -152,6 +152,14 @@ $(document).ready(function () {
         yaCounter38441125.reachGoal('HERO-MAIN-BUTTON');
     });
 
+    detachPrependTo("#video", "#youtube-video-desktop", "#youtube-video-mobile", 992, false, false);
+
+    $.extend(true, $(window).resize(), $(window).resize(
+        function () {
+            detachPrependTo("#video", "#youtube-video-desktop", "#youtube-video-mobile", 992, false, false);
+        }
+    ));
+
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
     /* - - - - - - - - - - - - - - - - PROBLEMS - - - - - - - - - - - - - - - */
 
@@ -704,3 +712,50 @@ $.extend({
 /*var allVars = $.getUrlVars();*/
 // Получит параметр URL по его имени
 /*var byName = $.getUrlVar('name');*/
+
+
+/// Перемещение объектов
+/// с условиями по ширине
+/// @obj - объект, который необходимо перенести
+/// @from - откуда производится перенос
+/// @to - куда производится перенос
+/// @width - при какой ширине производить перенос { < width }
+function detachPrependTo(obj, from, to, width, insertAfter, inverse) {
+    if (width === undefined) {
+        width = 768;
+    }
+    if (insertAfter === undefined) {
+        insertAfter = false;
+    }
+    if (inverse === undefined) {
+        inverse = false;
+    }
+
+    var condition;
+
+    if( inverse ) {
+        condition = getPageSize()[2] > width-1;
+    }
+    else {
+        condition = getPageSize()[2] < width;
+    }
+
+    if (condition) {
+        /* перемещаем контейнер */
+        if (insertAfter) {
+            $(from).find(obj).detach().insertAfter(to);
+        }
+        else {
+            $(from).find(obj).detach().prependTo(to);
+        }
+    }
+    else {
+        /* перемещаем контейнер */
+        if (insertAfter) {
+            $(to).siblings(obj).detach().prependTo(from);
+        }
+        else {
+            $(to).find(obj).detach().prependTo(from);
+        }
+    }
+}
