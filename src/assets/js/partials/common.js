6ced9131df8b26f78fa232664a00fb3aa75c8a0b
Ilya Purouski <MiKrob09@gmail.com>
2017-04-14 10:25:41 +0300

    Блок отзывов с VK спрятан по умолчанию. Добавлена кнопка показа этого блока .....

--- src/assets/js/partials/common.js
+++ src/assets/js/partials/common.js
@@ -306,6 +306,19 @@ $(document).ready(function () {
         });
     }
 
+
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    /* - - - - - - - - - - - - - - - - REVIEWS - - - - - - - - - - - - - - - */
+
+    /// Событие click() с отменой перехода по ссылке
+    $("#getMoreReviews").click(function (e) {
+        e.preventDefault();
+
+        $(this).fadeOut();
+
+        $("#reviewsVK").slideToggle();
+    });
+
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
     /* - - - - - - - - - - - - - - - - FOOTER - - - - - - - - - - - - - - */
 
