2c7e5d2258a8b4d96b3ced9f64babd142db080cb
Ilya Purouski <MiKrob09@gmail.com>
2017-06-16 19:53:16 +0300

    Обновлён fancybox

--- src/assets/js/plugins/fn_plug_fancybox.js
+++ src/assets/js/plugins/fn_plug_fancybox.js
@@ -8,148 +8,385 @@
 /// @prop: none.
 /// @return: none.
 
-var fn_plugins_fancybox = function () {
-    $("[data-fancybox]").fancybox({
-        /// Animation duration in ms
-        speed: 330,
 
-        // Enable infinite gallery navigation
-        loop: true,
-
-        // Should zoom animation change opacity, too
-        // If opacity is 'auto', then fade-out if image and thumbnail have different aspect ratios
-        opacity: 'auto',
-
-        // Space around image, ignored if zoomed-in or viewport smaller than 800px
-        margin: [44, 0],
-
-        // Horizontal space between slides
-        gutter: 30,
-
-        // Should display toolbars
-        infobar: true,
-        buttons: true,
-
-        // What buttons should appear in the toolbar
-        slideShow: true,
-        fullScreen: true,
-        thumbs: true,
-        closeBtn: true,
-
-        // Should apply small close button at top right corner of the content
-        // If 'auto' - will be set for content having type 'html', 'inline' or 'ajax'
-        smallBtn: 'auto',
-
-        image: {
-
-            // Wait for images to load before displaying
-            // Requires predefined image dimensions
-            // If 'auto' - will zoom in thumbnail if 'width' and 'height' attributes are found
-            preload: "auto",
-
-            // Protect an image from downloading by right-click
-            protect: true
-
-        },
-
-        ajax: {
-
-            // Object containing settings for ajax request
-            settings: {
-
-                // This helps to indicate that request comes from the modal
-                // Feel free to change naming
-                data: {
-                    fancybox: true
-                }
-            }
-
-        },
-
-        iframe: {
-
-            // Iframe template
-            tpl: '<iframe id="fancybox-frame{rnd}" name="fancybox-frame{rnd}" class="fancybox-iframe" frameborder="0" vspace="0" hspace="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen allowtransparency="true" src=""></iframe>',
-
-            // Preload iframe before displaying it
-            // This allows to calculate iframe content width and height
-            // (note: Due to "Same Origin Policy", you can't get cross domain data).
-            preload: true,
-
-            // Scrolling attribute for iframe tag
-            scrolling: 'no',
-
-            // Custom CSS styling for iframe wrapping element
-            css: {}
-
-        },
-
-        // Custom CSS class for layout
-        baseClass: '',
-
-        // Custom CSS class for slide element
-        slideClass: '',
-
-        // Base template for layout
-        baseTpl: '<div class="fancybox-container" role="dialog" tabindex="-1">' +
-            '<div class="fancybox-bg"></div>' +
-            '<div class="fancybox-controls">' +
-            '<div class="fancybox-infobar">' +
-            '<button data-fancybox-previous class="fancybox-button fancybox-button--left" title="Previous"></button>' +
-            '<div class="fancybox-infobar__body">' +
-            '<span class="js-fancybox-index"></span>&nbsp;/&nbsp;<span class="js-fancybox-count"></span>' +
-            '</div>' +
-            '<button data-fancybox-next class="fancybox-button fancybox-button--right" title="Next"></button>' +
-            '</div>' +
-            '<div class="fancybox-buttons">' +
-            '<button data-fancybox-close class="fancybox-button fancybox-button--close" title="Close (Esc)"></button>' +
-            '</div>' +
-            '</div>' +
-            '<div class="fancybox-slider-wrap">' +
-            '<div class="fancybox-slider"></div>' +
-            '</div>' +
-            '<div class="fancybox-caption-wrap"><div class="fancybox-caption"></div></div>' +
-            '</div>',
-
-        // Loading indicator template
-        spinnerTpl: '<div class="fancybox-loading"></div>',
-
-        // Error message template
-        errorTpl: '<div class="fancybox-error"><p>The requested content cannot be loaded. <br /> Please try again later.<p></div>',
-
-        // This will be appended to html content, if "smallBtn" option is not set to false
-        closeTpl: '<button data-fancybox-close class="fancybox-close-small"></button>',
-
-        // Container is injected into this element
-        parentEl: 'body',
-
-        // Enable gestures (tap, zoom, pan and pinch)
-        touch: true,
-
-        // Enable keyboard navigation
-        keyboard: true,
-
-        // Try to focus on first focusable element after opening
-        focus: true,
-
-        // Close when clicked outside of the content
-        closeClickOutside: true,
-
-        // Callbacks; See Documentation/API/Events for description and samples
-        beforeLoad: $.noop,
-        afterLoad: $.noop,
-        beforeMove: $.noop,
-        afterMove: $.noop,
-        onComplete: $.noop,
-
-        onInit: $.noop,
-        beforeClose: $.noop,
-        afterClose: $.noop,
-        onActivate: $.noop,
-        onDeactivate: $.noop
-    });
-};
 
 $(function () {
-    fn_plugins_fancybox;
+
+    /*
+
+    	Advanced example - Customized layout
+        ====================================
+
+    */
+
+    $('a[data-fancybox="cl-group"]').fancybox({
+        idleTime: false,
+        baseClass: 'fancybox-custom-layout',
+        margin: 0,
+        infobar: false,
+        thumbs: {
+            hideOnClose: false
+        },
+        touch: {
+            vertical: 'auto'
+        },
+        buttons: [
+            'close',
+            'thumbs',
+            'slideShow',
+            'fullScreen',
+        ],
+        animationEffect: false,
+        closeClickOutside: false,
+
+        // Customize caption area - append an advertisement
+        caption: function (instance) {
+            var advert = '<div class="ad"><p><a href="//fancyapps.com/fancybox/">fancyBox3</a> - touch enabled, responsive and fully customizable lightbox script</p></div>';
+
+            return advert + ($(this).data('caption') || '');
+        }
+    });
+
+
+
+    /*
+
+    	Advanced example - Morphing modal window
+
+        See demo on CodePen: http://codepen.io/fancyapps/pen/rmwyXN
+        ===========================================================
+
+    */
+
+
+    // Step 1: Create jQuery plugin
+    // ============================
+
+    $.fn.fancyMorph = function (opts) {
+
+        var Morphing = function ($btn, opts) {
+            var self = this;
+
+            self.opts = $.extend({
+                animationEffect: false,
+                infobar: false,
+                buttons: ['close'],
+                smallBtn: false,
+                touch: false,
+                baseClass: 'fancybox-morphing',
+                afterClose: function () {
+                    self.close();
+                }
+            }, opts);
+
+            self.init($btn);
+        };
+
+        Morphing.prototype.init = function ($btn) {
+            var self = this;
+
+            self.$btn = $btn.addClass('morphing-btn');
+
+            self.$clone = $('<div class="morphing-btn-clone" />')
+                .hide()
+                .insertAfter($btn);
+
+            // Add wrapping element and set initial width used for positioning
+            $btn.wrap('<span class="morphing-btn-wrap"></span>').on('click', function (e) {
+                e.preventDefault();
+
+                self.start();
+            });
+
+        };
+
+        Morphing.prototype.start = function () {
+            var self = this;
+
+            if (self.$btn.hasClass('morphing-btn_circle')) {
+                return;
+            }
+
+            // Set initial width, because it is not possible to start CSS transition from "auto"
+            self.$btn.width(self.$btn.width()).parent().width(self.$btn.outerWidth());
+
+            self.$btn.off('.fm').on("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function (e) {
+
+                if (e.originalEvent.propertyName === 'width') {
+                    self.$btn.off('.fm');
+
+                    self.animateBg();
+                }
+
+            }).addClass('morphing-btn_circle');
+
+        };
+
+        Morphing.prototype.animateBg = function () {
+            var self = this;
+
+            self.scaleBg();
+
+            self.$clone.show();
+
+            // Trigger repaint
+            self.$clone[0].offsetHeight;
+
+            self.$clone.off('.fm').on("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function (e) {
+                self.$clone.off('.fm');
+
+                self.complete();
+
+            }).addClass('morphing-btn-clone_visible');
+        };
+
+        Morphing.prototype.scaleBg = function () {
+            var self = this;
+
+            var $clone = self.$clone;
+            var scale = self.getScale();
+            var $btn = self.$btn;
+            var pos = $btn.offset();
+
+            $clone.css({
+                top: pos.top + $btn.outerHeight() * 0.5 - ($btn.outerHeight() * scale * 0.5) - $(window).scrollTop(),
+                left: pos.left + $btn.outerWidth() * 0.5 - ($btn.outerWidth() * scale * 0.5) - $(window).scrollLeft(),
+                width: $btn.outerWidth() * scale,
+                height: $btn.outerHeight() * scale,
+                transform: 'scale(' + (1 / scale) + ')'
+            });
+        };
+
+        Morphing.prototype.getScale = function () {
+            var $btn = this.$btn,
+                radius = $btn.outerWidth() * 0.5,
+                left = $btn.offset().left + radius - $(window).scrollLeft(),
+                top = $btn.offset().top + radius - $(window).scrollTop(),
+                windowW = $(window).width(),
+                windowH = $(window).height();
+
+            var maxDistHor = (left > windowW / 2) ? left : (windowW - left),
+                maxDistVert = (top > windowH / 2) ? top : (windowH - top);
+
+            return Math.ceil(Math.sqrt(Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2)) / radius);
+        };
+
+        Morphing.prototype.complete = function () {
+            var self = this;
+            var $btn = self.$btn;
+
+            $.fancybox.open({
+                src: $btn.data('src') || $btn.attr('href')
+            }, self.opts);
+        };
+
+        Morphing.prototype.close = function () {
+            var self = this;
+            var $clone = self.$clone;
+
+            self.scaleBg();
+
+            $clone.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function (e) {
+                $clone.hide();
+
+                self.$btn.removeClass('morphing-btn_circle');
+            });
+
+            $clone.removeClass('morphing-btn-clone_visible');
+
+            $(window).off('resize.fm');
+        };
+
+        // Init
+        this.each(function () {
+            var $this = $(this);
+
+            if (!$this.data("morphing")) {
+                $this.data("morphing", new Morphing($this, opts));
+            }
+
+        });
+
+        return this;
+    };
+
+
+    // Step 2: Start using it!
+    // =======================
+
+    $("[data-morphing]").fancyMorph({
+        hash: 'morphing'
+    });
+
+
+
+
+    /*
+
+        Advanced example - Confirm dialog
+
+        See demo on CodePen: http://codepen.io/fancyapps/pen/PmpePO
+        ===========================================================
+
+    */
+
+
+    // Step 1: Create reusable jQuery plugin
+    // =====================================
+
+    $.fancyConfirm = function (opts) {
+        opts = $.extend(true, {
+            title: 'Are you sure?',
+            message: '',
+            okButton: 'OK',
+            noButton: 'Cancel',
+            callback: $.noop
+        }, opts || {});
+
+        $.fancybox.open({
+            type: 'html',
+            src: '<div class="fc-content">' +
+                '<h3>' + opts.title + '</h3>' +
+                '<p>' + opts.message + '</p>' +
+                '<p class="tright">' +
+                '<a data-value="0" data-fancybox-close>' + opts.noButton + '</a>' +
+                '<button data-value="1" data-fancybox-close class="btn">' + opts.okButton + '</button>' +
+                '</p>' +
+                '</div>',
+            opts: {
+                animationDuration: 200,
+                animationEffect: 'material',
+                modal: true,
+                baseTpl: '<div class="fancybox-container fc-container" role="dialog" tabindex="-1">' +
+                    '<div class="fancybox-bg"></div>' +
+                    '<div class="fancybox-inner">' +
+                    '<div class="fancybox-stage"></div>' +
+                    '</div>' +
+                    '</div>',
+                afterClose: function (instance, current, e) {
+                    var button = e ? e.target || e.currentTarget : null;
+                    var value = button ? $(button).data('value') : 0;
+
+                    opts.callback(value);
+                }
+            }
+        });
+    }
+
+    // Step 2: Start using it!
+    // =======================
+
+    $("#test_confirm").click(function () {
+
+        // Open customized confirmation dialog window
+        $.fancyConfirm({
+            title: "Use Google's location service?",
+            message: "Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running",
+            okButton: 'Agree',
+            noButton: 'Disagree',
+            callback: function (value) {
+                if (value) {
+                    $("#test_confirm_rez").html("Let's do this!");
+
+                } else {
+                    $("#test_confirm_rez").html("Maybe later.");
+                }
+            }
+        });
+
+    });
+
+
+    /*
+
+    	Advanced example - Product quick view
+        =====================================
+
+    */
+
+    $(".quick_view").fancybox({
+        baseClass: 'quick-view-container',
+        infobar: false,
+        buttons: false,
+        thumbs: false,
+        margin: 0,
+        touch: {
+            vertical: false
+        },
+        animationEffect: false,
+        transitionEffect: "slide",
+        transitionDuration: 500,
+        baseTpl: '<div class="fancybox-container" role="dialog">' +
+            '<div class="quick-view-content">' +
+            '<div class="quick-view-carousel">' +
+            '<div class="fancybox-stage"></div>' +
+            '</div>' +
+            '<div class="quick-view-aside"></div>' +
+            '<button data-fancybox-close class="quick-view-close">X</button>' +
+            '</div>' +
+            '</div>',
+
+        onInit: function (instance) {
+
+            /*
+
+                #1 Create bullet navigation links
+                =================================
+
+            */
+
+            var bullets = '<ul class="quick-view-bullets">';
+
+            for (var i = 0; i < instance.group.length; i++) {
+                bullets += '<li><a data-index="' + i + '" href="javascript:;"><span>' + (i + 1) + '</span></a></li>';
+            }
+
+            bullets += '</ul>';
+
+            $(bullets).on('click touchstart', 'a', function () {
+                    var index = $(this).data('index');
+
+                    $.fancybox.getInstance(function () {
+                        this.jumpTo(index);
+                    });
+
+                })
+                .appendTo(instance.$refs.container.find('.quick-view-carousel'));
+
+
+            /*
+
+                #2 Add product form
+                ===================
+
+            */
+
+            var $element = instance.group[instance.currIndex].opts.$orig;
+            var form_id = $element.data('qw-form');
+
+            instance.$refs.container.find('.quick-view-aside').append(
+
+                // In this example, this element contains the form
+                $('#' + form_id).clone(true).removeClass('hidden')
+            );
+
+        },
+
+        beforeShow: function (instance) {
+
+            /*
+                Mark current bullet navigation link as active
+            */
+
+            instance.$refs.container.find('.quick-view-bullets')
+                .children()
+                .removeClass('active')
+                .eq(instance.currIndex)
+                .addClass('active');
+
+        }
+
+    });
+
+
 });
