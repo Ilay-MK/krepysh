bc180250e05e6007192907fa1424c13aab9b6bb3
Ilya Purouski <MiKrob09@gmail.com>
2017-06-19 00:14:30 +0300

    Fancybox - над лид-формой добавлен заголовок "записаться на пробное занятие" + фон белый, цвет чёрный

--- src/assets/js/plugins/fn_plug_fancybox.js
+++ src/assets/js/plugins/fn_plug_fancybox.js
@@ -90,7 +90,7 @@ $(function () {
 
         // Customize caption area - append an advertisement
         caption: function (instance) {
-            var advert = '<div class="ad"><button type="button" class="btn btn-primary1 center-block" data-toggle="modal" data-target="#modalOrder" data-service="Галерея" role="button" onclick="$.fancybox.close();"><span>Оставить заявку</span></button></div>';
+            var advert = '<div class="ad"><div class="h5 text-uppercase text-center">Записаться на пробное занятие</div><button type="button" class="btn btn-primary1 center-block" data-toggle="modal" data-target="#modalOrder" data-service="Галерея" role="button" onclick="$.fancybox.close();"><span>Оставить заявку</span></button></div>';
 
             return advert + ($(this).data('caption') || '');
         }
