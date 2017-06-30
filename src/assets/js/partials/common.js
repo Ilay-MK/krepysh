ca53db52f2076121589ca7603aaf31f82e1610f5
Ilya Purouski <MiKrob09@gmail.com>
2017-06-30 16:31:18 +0300

    Упразнить отправку ричгол

--- src/assets/js/partials/common.js
+++ src/assets/js/partials/common.js
@@ -132,7 +132,7 @@ $(document).ready(function () {
         $("#header .container-bg").css("background-image", "url('assets/img/header/bg_3-2.jpg')");
     }, 4000);
 
-    $("#callMe button").click(function () {
+    /*$("#callMe button").click(function () {
         yaCounter38441125.reachGoal('TOP-CALLME-BUTTON');
     });
 
@@ -150,7 +150,7 @@ $(document).ready(function () {
 
     $("#hero-main-btn").click(function () {
         yaCounter38441125.reachGoal('HERO-MAIN-BUTTON');
-    });
+    });*/
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
     /* - - - - - - - - - - - - - - - - PROBLEMS - - - - - - - - - - - - - - - */
@@ -227,16 +227,16 @@ $(document).ready(function () {
         });
     }
 
-    $("#coaches-main-button").click(function () {
+    /*$("#coaches-main-button").click(function () {
         yaCounter38441125.reachGoal('COACHES-MAIN-BUTTON');
-    });
+    });*/
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
     /* - - - - - - - - - - - - - - - PRICE - - - - - - - - - - - - - - - */
 
-    $("#price-main-button").click(function () {
+    /*$("#price-main-button").click(function () {
         yaCounter38441125.reachGoal('PRICE-MAIN-BUTTON');
-    });
+    });*/
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
     /* - - - - - - - - - - - - - - - SERVICES - - - - - - - - - - - - - - - */
@@ -272,7 +272,7 @@ $(document).ready(function () {
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
     /* - - - - - - - - - - - - - - - - FOOTER - - - - - - - - - - - - - - */
 
-    $("#footer-phone-number-1").click(function () {
+    /*$("#footer-phone-number-1").click(function () {
         yaCounter38441125.reachGoal('FOOTER-PHONE-NUMBER-1-BUTTON');
     });
 
@@ -282,7 +282,7 @@ $(document).ready(function () {
 
     $("#footer-email").click(function () {
         yaCounter38441125.reachGoal('FOOTER-EMAIL-BUTTON');
-    });
+    });*/
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
     /* - - - - - - - - - - - - - - - - - COMMON - - - - - - - - - - - - - -  */
@@ -656,7 +656,7 @@ function ajax(ob) {
             /*$(ob).find('.form-control-feedback-message-success').addClass("flex-center");*/
             /*result.removeClass("text-danger bg-danger text-success bg-success").text("");*/
             /*$(ob).find('.submit').prop('disabled', false);*/
-            yaCounter38441125.reachGoal('ORDER'); /* for Y. target */
+            /*yaCounter38441125.reachGoal('ORDER');*/ /* for Y. target */
             $(ob).find('#whichService').val(""); /* нужно заменить на класс */
             $(ob).find('.has-feedback').removeClass('has-success');
             $(ob).find('.form-control-feedback').removeClass('glyphicon-ok');
